0abfeb9ad80b4faa32d223b239a584785e9bc55c
Katarzyna Werner <katarzyna.werner1234@gmail.com>
2025-01-17 15:58:59 +0100

    change some breakpoints nad swiper classes names

--- 30144552-sky-high/scripts.js
+++ 30144552-sky-high/scripts.js
@@ -1,46 +1,46 @@
-const swiper = new Swiper('.swiper1', {
-            // Optional parameters
-            loop: true,
-            autoHeight: true,
-            // autoplay: true,
+const swiper = new Swiper('.es-swiper1', {
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
+    // autoplay: true,
 
-            pagination: {
-                el: '.swiper1-pagination',
-                clickable: true,
-            },
+    pagination: {
+        el: '.es-swiper1-pagination',
+        clickable: true,
+    },
 
-            navigation: {
-                nextEl: '.swiper1-button-next',
-                prevEl: '.swiper1-button-prev',
-            },
-        });
-        const swiper2 = new Swiper('.swiper2', {
-            // Optional parameters
-            loop: true,
-            autoHeight: true,
-            // autoplay: true,
+    navigation: {
+        nextEl: '.es-swiper1-button-next',
+        prevEl: '.es-swiper1-button-prev',
+    },
+});
+const swiper2 = new Swiper('.es-swiper2', {
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
+    // autoplay: true,
 
-            pagination: {
-                el: '.swiper2-pagination',
-                clickable: true,
-            },
+    pagination: {
+        el: '.es-swiper2-pagination',
+        clickable: true,
+    },
 
-            navigation: {
-                nextEl: '.swiper2-button-next',
-                prevEl: '.swiper2-button-prev',
-            },
-            breakpoints: {
-                0: { // For screens 0px and wider
-                    slidesPerView: 1,
-                    spaceBetween: 4,
-                },
-                576: { // For screens 576px and wider
-                    slidesPerView: 2,
-                    spaceBetween: 4,
-                },
-                1601: { // For screens 1601px and wider
-                    slidesPerView: 3,
-                    spaceBetween: 4,
-                },
-            },
-        });
+    navigation: {
+        nextEl: '.es-swiper2-button-next',
+        prevEl: '.es-swiper2-button-prev',
+    },
+    breakpoints: {
+        0: { // For screens 0px and wider
+            slidesPerView: 1,
+            spaceBetween: 4,
+        },
+        576: { // For screens 576px and wider
+            slidesPerView: 2,
+            spaceBetween: 4,
+        },
+        1601: { // For screens 1601px and wider
+            slidesPerView: 3,
+            spaceBetween: 4,
+        },
+    },
+});
